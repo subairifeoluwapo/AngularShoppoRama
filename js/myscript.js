ccdcdab410c06e0200e4731f9c2931c9716f3eac
subairifeoluwapo <subairifeoluwapo@gmail.com>
2014-09-29 15:48:37 +0100

    new

--- js/myscript.js
+++ js/myscript.js
@@ -1,85 +1,80 @@
 
-var app = angular.module('AngularShoppoRama', []);
+var app = angular.module('AngularShoppoRama', []).config(function($sceProvider){
+	$sceProvider.enabled(false);
+});
 
-app.controller('sectionController', ['$scope','$http', function($scope, $http){
+app.controller('sectionController', ['$scope','$http', function($scope, $http) {
 
-	$scope.primaryFunction = function(){	
-		var lat;
-		var lon;
-		// $scope.map = {center: {latitude: 6.441158, longitude: 3.417977}, zoom: 13};
-		// $scope.options = {scrollwheel: false};	
+	$scope.primaryFunction = function(){
 		if(navigator.geolocation){	
 			navigator.geolocation.getCurrentPosition(function(position){
 				console.log(position);
-				lat = position.coords.latitude;
-				lon = position.coords.longitude;
+				var lat = position.coords.latitude;
+				var lon = position.coords.longitude;
 				console.log(lat);
 				console.log(lon);
-
-				$scope.onStartShowMap = function(){
-					var test1 = {
-						params: {
-							url: 'https://www.google.com/maps/embed/v1/view?key=AIzaSyDzBjNNTZDL-eYH_Nbth3IMZTcGN3PR7aw&zoom=11&maptype=satellite',
-							q: position.coords.latitude + ',' + position.coords.longitude
-						}
-					};
-					console.log(test1.params);
-					$scope.loc = test1;
-				};
+				$scope.latlng = lat + ',' + lon;
 				$scope.onStartShowMap();
-
-				$scope.fetchLocations = function() {
-					var url = 'https://api.foursquare.com/v2/venues/explore';
-					var config = {
-							params : {
-								client_id: 'HBMVGNAHBQMV5A0FM3RLUM1NIXXZ1CU4XXOLCOP4GLXGGBHC', 
-								client_secret: 'Q4RXJT4IZVWALD1HIOWNKXVUPVJZBUJ5JEPDU4HYD1KVF1GD', 
-								section: 'shops', 
-								v: '20130815', 
-								callback : 'JSON_CALLBACK'
-							}
-					};
-					config.params.ll = lat + ',' + lon;
-					console.log(config.params.ll);
-					$http.jsonp(url, config).success(function(response){
-						console.log(response.response.groups[0].items);
-						var post = response.response.groups[0];
-						$scope.shops = post.items;
-
-						$scope.fetchMapLocation = function(){
-							var test2 = {
-									params:	{
-										url: 'https://www.google.com/maps/embed/v1/place?key=AIzaSyDzBjNNTZDL-eYH_Nbth3IMZTcGN3PR7aw&zoom=11&maptype=roadmap&q='
-									}
-							}
-							console.log(test2.params);
-							$scope.loc2 = test2;
-						};
-					});	
-				};
-
-				$scope.fetchWeatherCondition = function(){
-					var url = 'https://api.worldweatheronline.com/free/v1/weather.ashx';
-					var config = {
-						params : {
-							format: 'json',
-							key: 'f83676bdb9c2eee1f780967602c8faf1fd4d1cb9',
-							callback: 'JSON_CALLBACK'
-						}
-					};
-					config.params.q = lat + ',' + lon;
-					console.log(config.params.q);
-					$http.jsonp(url, config).success(function(response){
-						console.log(response);
-						$scope.conditions = response;
-					});
-				};
 			});
 		}	else {alert("Geolocation is not supported by this browser.");
 			};
-		$scope.reloadPage = function() {
-	   		window.location.reload();
-		};
 	};
 	$scope.primaryFunction();
+
+	$scope.onStartShowMap = function(){
+		$scope.url1 = 'https://www.google.com/maps/embed/v1/view?key=AIzaSyDzBjNNTZDL-eYH_Nbth3IMZTcGN3PR7aw&zoom=3&maptype=satellite&center=' + $scope.latlng;
+		console.log($scope.url1);
+	};
+
+
+	$scope.fetchLocations = function() {
+		var url = 'https://api.foursquare.com/v2/venues/explore';
+		var config = {
+				params : {
+					client_id: 'HBMVGNAHBQMV5A0FM3RLUM1NIXXZ1CU4XXOLCOP4GLXGGBHC', 
+					client_secret: 'Q4RXJT4IZVWALD1HIOWNKXVUPVJZBUJ5JEPDU4HYD1KVF1GD', 
+					section: 'shops', 
+					v: '20130815', 
+					venuephotos: 1,
+					callback : 'JSON_CALLBACK'
+				}
+			};
+
+		config.params.ll = $scope.latlng;
+		console.log(config.params.ll);
+		$http.jsonp(url, config).success(function(response){
+			console.log(response.response);
+			var post = response.response.groups[0];
+			$scope.shops = post.items;
+			$scope.url2 = 'https://www.google.com/maps/embed/v1/place?key=AIzaSyDzBjNNTZDL-eYH_Nbth3IMZTcGN3PR7aw&zoom=11&maptype=roadmap&q=lagos';
+		});	
+	};
+
+
+	$scope.fetchWeatherCondition = function(){
+		var urll = 'https://api.worldweatheronline.com/free/v1/weather.ashx';
+		var config2 = {
+			params : {
+				format: 'json',
+				key: 'f83676bdb9c2eee1f780967602c8faf1fd4d1cb9',
+				callback: 'JSON_CALLBACK'
+			}
+		};
+		config2.params.q = $scope.latlng;
+		console.log(config2.params.q);
+		$http.jsonp(urll, config2).success(function(response){
+			console.log(response);
+			$scope.conditions = response;
+		});
+	};
+
+
+	$scope.reloadPage = function() {
+	   	window.location.reload();
+	};
+
+	
+	$scope.fetchMapLocation = function(){
+		$scope.showMe = 2 + 2;
+	};
 }]);
